c3f68ddf3ea89b77a8a4e306ce36265f2fb29374
bene-labs <benedikt.mertes@epitech.eu>
2025-12-15 09:53:46 +0100

    Update day11.py

--- day11.py
+++ day11.py
@@ -1,24 +1,18 @@
 ﻿from functools import cache
 
 
-class Node:
-    def __init__(self, connected_nodes: list, name: str):
-        self.connected_nodes = connected_nodes
-        self.id : str = name
-
-
-def count_paths_to(nodes, start: str, end: str) -> int:
-
+def count_paths_to(nodes: dict[str, list[str]], start: str, end: str) -> int:
+    
     @cache
     def _count_paths_to_rec(current: str, goal: str)-> int:
         if current == goal:
             return 1
-        return sum([_count_paths_to_rec(next_node.id, goal) for next_node in nodes[current].connected_nodes])
+        return sum([_count_paths_to_rec(next_node, goal) for next_node in nodes[current]])
 
     return _count_paths_to_rec(start, end)
 
 
-def count_paths_along(nodes, start: str, end: str, to_visit: frozenset[str]) -> int:
+def count_paths_along(nodes: dict[str, list[str]], start: str, end: str, to_visit: frozenset[str]) -> int:
 
     @cache
     def _count_paths_along_rec(current: str, goal: str, must_visit: frozenset[str])-> int:
@@ -26,25 +20,26 @@ def count_paths_along(nodes, start: str, end: str, to_visit: frozenset[str]) -> 
             must_visit = must_visit - {current}
         elif current == goal:
             return 1 if len(must_visit) == 0 else 0
-        return sum([_count_paths_along_rec(next_node.id, goal, must_visit) for next_node in nodes[current].connected_nodes])
+        return sum([_count_paths_along_rec(next_node, goal, must_visit) for next_node in nodes[current]])
 
     return _count_paths_along_rec(start, end, to_visit)
 
 
-def get_nodes(input_path: str)->dict[str, Node]:
-    unique_nodes: dict[str, Node] = {}
+def get_nodes(input_path: str)->dict[str, list[str]]:
+    unique_nodes: dict[str, list[str]] = {}
     with open(input_path, 'r', encoding='utf-8-sig') as input_file:
         for line in input_file.read().splitlines():
             name, connected_to = line.split(':')
             connected_nodes = []
             for node in connected_to.strip().split(' '):
                 if not node in unique_nodes:
-                    unique_nodes[node] = Node([], node)
-                connected_nodes.append(unique_nodes[node])
+                    unique_nodes[node] = []
+                connected_nodes.append(node)
             if name in unique_nodes:
-                unique_nodes[name].connected_nodes += connected_nodes
+                unique_nodes[name] += connected_nodes
             else:
-                unique_nodes[name] = Node(connected_nodes, name)
+                unique_nodes[name] = connected_nodes
+    
     return unique_nodes
 
 
